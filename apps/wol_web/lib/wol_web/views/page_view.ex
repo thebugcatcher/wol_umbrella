87f467df978297f310e8d63ec75faf6f34ece2ef
Adi Iyengar <aditya7iyengar@gmail.com>
2017-09-23 15:29:05 -0400

    Add helpers for wheel decide url

--- apps/wol_web/lib/wol_web/views/page_view.ex
+++ apps/wol_web/lib/wol_web/views/page_view.ex
@@ -1,3 +1,22 @@
 defmodule WolWeb.PageView do
   use WolWeb, :view
+
+  @base_url "http://wheeldecide.com/e.php?"
+  @end_url "col=wol&t=Participant+1&time=5"
+
+  def form_wheel_url(people) do
+    people
+    |> Enum.reduce({@base_url, 1}, fn(person, {url, num}) ->
+      {url <> "c#{num}=" <> trim_name(person) <> "&", num + 1}
+    end)
+    |> elem(0)
+    |> (& &1 <> @end_url).()
+  end
+
+  defp trim_name(person) do
+    person
+    |> (& &1.name).()
+    |> String.split(" ")
+    |> Enum.join("-")
+  end
 end
