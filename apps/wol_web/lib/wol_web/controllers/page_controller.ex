cfc2f62cd3781258d877b1707dfb4e8def5b1070
Adi Iyengar <aditya7iyengar@gmail.com>
2017-09-23 13:54:50 -0400

    Add more actions to page controllers for spins

--- apps/wol_web/lib/wol_web/controllers/page_controller.ex
+++ apps/wol_web/lib/wol_web/controllers/page_controller.ex
@@ -1,7 +1,31 @@
 defmodule WolWeb.PageController do
   use WolWeb, :controller
 
+  alias Wol.Organization
+
   def index(conn, _params) do
-    render conn, "index.html"
+    people = Organization.list_people()
+
+    render conn,
+      "index.html",
+      spin1_people: people,
+      spin2_people: []
+  end
+
+  def about(conn, _params) do
+    render conn, "about.html"
+  end
+
+  def spin1(conn, _params) do
+    people = Organization.list_people()
+
+    render conn,
+      "index.html",
+      spin1_people: :done,
+      spin2_people: people
+  end
+
+  def spin2(conn, _params) do
+
   end
 end
