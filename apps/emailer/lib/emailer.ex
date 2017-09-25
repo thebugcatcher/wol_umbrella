afa91b445cbccb900359521a033b5fbc31c1908e
Adi Iyengar <aditya7iyengar@gmail.com>
2017-09-24 22:05:32 -0400

    Add emailer app

---
+++ apps/emailer/lib/emailer.ex
@@ -0,0 +1,18 @@
+defmodule Emailer do
+  @moduledoc """
+  Documentation for Emailer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Emailer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
