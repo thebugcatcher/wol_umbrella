2cb8624aeaadf63b3cfab9b2c8f36632c2a93f41
Adi Iyengar <aditya7iyengar@gmail.com>
2017-09-26 08:04:09 -0400

    Update views:
    - Put pages inside a container
    - Add ability to add/edit/remove mentors

--- apps/wol_web/lib/wol_web/controllers/person_controller.ex
+++ apps/wol_web/lib/wol_web/controllers/person_controller.ex
@@ -1,8 +1,10 @@
 defmodule WolWeb.PersonController do
   use WolWeb, :controller
 
-  alias Wol.Organization
-  alias Wol.Organization.Person
+  import Ecto.Query
+
+  alias Wol.{Organization, Repo}
+  alias Organization.{Person, Relationship}
 
   def index(conn, _params) do
     people = Organization.list_people()
@@ -11,12 +13,19 @@ defmodule WolWeb.PersonController do
 
   def new(conn, _params) do
     changeset = Organization.change_person(%Person{})
-    render(conn, "new.html", changeset: changeset)
+    people = Organization.list_people()
+    render(conn, "new.html", changeset: changeset, people: people)
   end
 
   def create(conn, %{"person" => person_params}) do
+    mentor_name = Map.get(person_params, "mentor")
+    supervisor_name = Map.get(person_params, "supervisor")
+
     case Organization.create_person(person_params) do
       {:ok, person} ->
+        create_relationship(person, mentor_name, "mentor")
+        create_relationship(person, supervisor_name, "supervisor")
+
         conn
         |> put_flash(:info, "Person created successfully.")
         |> redirect(to: person_path(conn, :show, person))
@@ -25,22 +34,53 @@ defmodule WolWeb.PersonController do
     end
   end
 
+  defp create_relationship(_person, "", _relation), do: {:ok, :done}
+
+  defp create_relationship(person, relative_name, relation) do
+    relative = Wol.Repo.get_by!(Organization.Person, [name: relative_name])
+
+    Relationship.changeset(%Relationship{}, %{
+      person2_id: person.id,
+      person1_id: relative.id,
+      relationship_type: relation})
+    |> Wol.Repo.insert!()
+  end
+
   def show(conn, %{"id" => id}) do
     person = Organization.get_person!(id)
-    render(conn, "show.html", person: person)
+    mentor = get_relative(person, "mentor")
+    supervisor = get_relative(person, "supervisor")
+
+    render(conn, "show.html", person: person, mentor: mentor, supervisor: supervisor)
+  end
+
+  defp get_relative(person, relation) do
+    Relationship
+    |> join(:inner, [r], p in Person, r.person1_id == p.id)
+    |> where([r], r.person2_id == ^person.id)
+    |> where([r], r.relationship_type == ^relation)
+    |> last(:inserted_at)
+    |> select([r, p], p)
+    |> Repo.one()
   end
 
   def edit(conn, %{"id" => id}) do
     person = Organization.get_person!(id)
+    people = Organization.list_people()
     changeset = Organization.change_person(person)
-    render(conn, "edit.html", person: person, changeset: changeset)
+    render(conn, "edit.html", person: person, changeset: changeset, people: people)
   end
 
   def update(conn, %{"id" => id, "person" => person_params}) do
     person = Organization.get_person!(id)
+    mentor_name = Map.get(person_params, "mentor")
+    supervisor_name = Map.get(person_params, "supervisor")
 
     case Organization.update_person(person, person_params) do
       {:ok, person} ->
+        create_relationship(person, mentor_name, "mentor")
+        create_relationship(person, supervisor_name, "supervisor")
+
         conn
         |> put_flash(:info, "Person updated successfully.")
         |> redirect(to: person_path(conn, :show, person))
