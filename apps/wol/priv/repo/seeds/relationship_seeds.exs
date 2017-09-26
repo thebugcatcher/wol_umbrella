856fc4ba6f4b73233409dc40b1cfba412018a964
Adi Iyengar <aditya7iyengar@gmail.com>
2017-09-26 11:20:09 -0400

    Update Relationsihp seeds accordingly

--- apps/wol/priv/repo/seeds/relationship_seeds.exs
+++ apps/wol/priv/repo/seeds/relationship_seeds.exs
@@ -1,19 +1,14 @@
 alias Wol.{Repo, Organization.Person, Organization.Relationship}
 
 adi = Repo.get_by(Person, email: "adi.iyengar@annkissam.com")
-allison = Repo.get_by(Person, email: "allison.dyer@annkissam.com")
-clarence = Repo.get_by(Person, email: "clarence.lee@annkissam.com")
-david = Repo.get_by(Person, email: "david.argueta@annkissam.com")
+dana = Repo.get_by(Person, email: "dana.lewis@annkissam.com")
 josh = Repo.get_by(Person, email: "josh.adams@annkissam.com")
-ryan = Repo.get_by(Person, email: "ryan.vasios@annkissam.com")
-zoe = Repo.get_by(Person, email: "zoe.carlberg@annkissam.com")
+matteo = Repo.get_by(Person, email: "matteo.ramosmucci@annkissam.com")
+sarah = Repo.get_by(Person, email: "sarah.kulik@annkissam.com")
 
 Repo.insert!(%Relationship{person1_id: josh.id, person2_id: adi.id, relationship_type: "supervisor"})
-Repo.insert!(%Relationship{person1_id: josh.id, person2_id: allison.id, relationship_type: "supervisor"})
-Repo.insert!(%Relationship{person1_id: josh.id, person2_id: clarence.id, relationship_type: "supervisor"})
-Repo.insert!(%Relationship{person1_id: josh.id, person2_id: david.id, relationship_type: "supervisor"})
-Repo.insert!(%Relationship{person1_id: josh.id, person2_id: zoe.id, relationship_type: "supervisor"})
-Repo.insert!(%Relationship{person1_id: david.id, person2_id: ryan.id, relationship_type: "supervisor"})
-Repo.insert!(%Relationship{person1_id: david.id, person2_id: ryan.id, relationship_type: "mentor"})
-Repo.insert!(%Relationship{person1_id: zoe.id, person2_id: clarence.id, relationship_type: "mentor"})
+Repo.insert!(%Relationship{person1_id: matteo.id, person2_id: dana.id, relationship_type: "supervisor"})
+Repo.insert!(%Relationship{person1_id: matteo.id, person2_id: dana.id, relationship_type: "mentor"})
+Repo.insert!(%Relationship{person1_id: matteo.id, person2_id: sarah.id, relationship_type: "supervisor"})
+Repo.insert!(%Relationship{person1_id: matteo.id, person2_id: sarah.id, relationship_type: "mentor"})
 
